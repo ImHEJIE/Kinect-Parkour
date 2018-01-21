214f7d2a89d8d22d0ffc54b8976f43ddfe9576bb
AsanCai <969850420@qq.com>
2018-01-21 21:41:34 +0800

    修复MenuController缺失的问题

---
+++ Assets/Scripts/MenuScene/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    private GameObject instruction;
+    private GameObject menu;
+
+    private void Awake() {
+        instruction = GameObject.Find("Instruction");
+        menu = GameObject.Find("Menu");
+    }
+
+    void Start() {
+        instruction.SetActive(false);
+    }
+
+    public void StartBtn() {
+        SceneManager.LoadScene("main", LoadSceneMode.Single);
+    }
+
+    public void InstructionBtn() {
+        menu.SetActive(false);
+
+        instruction.SetActive(true);
+    }
+
+    public void ExitBtn() {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    public void CloseBtn() {
+        menu.SetActive(true);
+
+        instruction.SetActive(false);
+    }
+}
