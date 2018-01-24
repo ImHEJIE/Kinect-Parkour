1aeed6b2fe6a82afa9e44b5550662c6bdaa9d536
ojf1996 <247057911@qq.com>
2018-01-24 15:14:07 +0800

    修改collider

---
+++ Assets/GamingBGMController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamingBGMController : MonoBehaviour {
+
+    //音轨列表
+    public AudioClip[] clips;
+
+    //音源
+    public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
